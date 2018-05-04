a5733ef3d9ee1af58f17026601805e80247bb859
osamu yamakami <oosamuuy@gmail.com>
2018-05-04 14:56:49 +0900

    class作成

---
+++ src/Laravel55JaHelper/Helper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Laravel55JaHelper;
+
+class Helper
+{
+    public function __construct() {
+        
+
+        };
+    }
+
+
+    public function helloHelper() {
+        
+        
+        return "Hello Helper for Laravel55Ja";
+    }
+    
+}
