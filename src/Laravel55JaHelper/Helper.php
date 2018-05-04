9bf473bd79a38d025d8bea79d400eba0963540a4
osamu yamakami <oosamuuy@gmail.com>
2018-05-05 08:36:50 +0900

    gitignore update

--- src/Laravel55JaHelper/Helper.php
+++ src/Laravel55JaHelper/Helper.php
@@ -17,4 +17,29 @@ class Helper
         return "Hello Helper for Laravel55Ja";
     }
     
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Method makeUniqueid
+    |--------------------------------------------------------------------------
+    | make uniqeid
+    | * @access public
+    | * @param STIRINGS $timestamp
+    | * @return $timestamp
+    | * @throws
+    | * @todo
+    |
+    */
+    public function makeUniqueid($oiprefix="000") {
+
+        $uniqueid_tmp = uniqid();
+        // (example)5-94f5-3b53-8eda
+        $uiprefix = substr($uniqueid_tmp,0,1);
+        $ui1 = substr($uniqueid_tmp,1,4);
+        $ui2 = substr($uniqueid_tmp,5,4);
+        $ui3 = substr($uniqueid_tmp,9,4);
+        $uniqueid = $oiprefix."-".$uiprefix."-".$ui1."-".$ui2."-".$ui3;
+
+        return $uniqueid;
+    }
 }
