7473029933481b137c0ddd889615b036d5dff612
osamuya <yamakami@3kaku.co.jp>
2018-05-11 00:05:39 +0900

    useragent test

--- src/Laravel55JaHelper/Helper.php
+++ src/Laravel55JaHelper/Helper.php
@@ -2,6 +2,9 @@
 
 namespace Laravel55JaHelper;
 
+
+use Laravel55JaHelper\Useragent\Useragent;
+
 class Helper
 {
     public function __construct() {
@@ -17,4 +20,25 @@ class Helper
         return "Hello Helper for Laravel55Ja";
     }
     
+	
+	
+	
+/**
+ * User agent judgment
+ *
+ * $option null        -> Simply judge whether it is pc | sp(smartphone)
+ * $option 'type'      -> Judge whether it is pc | pad | sp(smartphone)
+ * $option 'useragent' -> Determine pc | android | ios
+ * $option 'device'    -> Detailed device determination
+ * | Windows|Macintosh|iPhone|iPad|iPod|Android|AndroidTablet|WindowsPhone|false(No match) |
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+	static public function ua($option = null)
+	{
+//		$ua = new Useragent();
+		echo Useragent::helloUseragent();
+
+		return "foo";
+	}
 }
