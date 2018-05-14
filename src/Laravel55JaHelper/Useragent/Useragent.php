b4855709bbf69bc5b2401998f3fcb1170bb43a37
sankaku.osamuya <oosamuuy@gmail.com>
2018-05-14 19:43:15 +0900

    なんでgit消えるの？

---
+++ src/Laravel55JaHelper/Useragent/Useragent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Laravel55JaHelper\Useragent;
+
+class Useragent
+{
+	
+	
+	static function helloUseragent() {
+		
+		
+		
+		return "Hello Useragent for Laravel55Ja";
+	}
+	
+	
+	
+	
+	
+	
+}
